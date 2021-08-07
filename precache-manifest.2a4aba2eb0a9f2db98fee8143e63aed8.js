05c9c9727bcac968c71b792b7a290f1d18476010
IJAS A H <41640210+ijasah@users.noreply.github.com>
2021-08-07 20:40:41 +0530

    Updates

---
+++ precache-manifest.2a4aba2eb0a9f2db98fee8143e63aed8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73401fa268dcc878de1e8251a7ef96cf",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "e730c451bdf6edab70f0",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f84f9610aab83f3dc35f",
+    "url": "/home/static/js/2.46a1e027.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.46a1e027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e730c451bdf6edab70f0",
+    "url": "/home/static/js/main.e23aa69e.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "56c9b58cab87682ab04a4b32d2d2f63b",
+    "url": "/home/static/media/Analytics_vidhya.56c9b58c.png"
+  },
+  {
+    "revision": "2fd7ef42bd717f8eb63ff39131e1ac36",
+    "url": "/home/static/media/Ijas.2fd7ef42.jpg"
+  },
+  {
+    "revision": "25404f737e6e81694da3202d92659fbd",
+    "url": "/home/static/media/ihrd.25404f73.jpeg"
+  },
+  {
+    "revision": "6dc2e741efaa7e4df1ba49a7597fc284",
+    "url": "/home/static/media/infosys.6dc2e741.png"
+  },
+  {
+    "revision": "2fb0cf2e79c2146bdd50aa4116fa2acd",
+    "url": "/home/static/media/project1.2fb0cf2e.jpg"
+  },
+  {
+    "revision": "d0b445613e1d7632408db534d6c921fe",
+    "url": "/home/static/media/project2.d0b44561.jpg"
+  },
+  {
+    "revision": "850ee75a0fe7eceee43605647df4ea3f",
+    "url": "/home/static/media/project3.850ee75a.jpg"
+  },
+  {
+    "revision": "193c7ed0f74733f6233f82c48572bc45",
+    "url": "/home/static/media/resume.193c7ed0.pdf"
+  },
+  {
+    "revision": "0897251a1282b173b10717e800b95948",
+    "url": "/home/static/media/sayone.0897251a.png"
+  }
+]);
